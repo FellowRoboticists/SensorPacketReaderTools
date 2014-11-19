53cf60bc51796df088fae0defad130edadfdf9ff
Dave Sieh <dsieh@providigm.com>
2014-11-19 15:28:33 -0700

    Added value reader method.

--- src/com/naiveroboticist/sensor/SensorPacketReader.java
+++ src/com/naiveroboticist/sensor/SensorPacketReader.java
@@ -1,8 +1,52 @@
 package com.naiveroboticist.sensor;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class SensorPacketReader {
+	
+	/**
+	 * Sensor packet, payload size values
+	 */
+	private static final int[] PACKET_PAYLOAD_SIZES = {
+		0, 0, 0, 0, 0, 0, 0, // Not used
+		1, // * Bumps and Wheel Drops -  7 - 1 byte
+	    1, // * Wall                     8 - 1 byte
+	    1, // * Cliff Left               9 - 1 byte
+	    1, // * Cliff Front Left        10 - 1 byte
+	    1, // * Cliff Front Right       11 - 1 byte
+	    1, // * Cliff Right             12 - 1 byte
+	    1, // * Virtual Wall            13 - 1 byte
+	    1, // * Low Side Driver UnderC  14 - 1 byte
+	    1, // * Unused bytes            15 - 1 byte
+	    1, // * Unused bytes            16 - 1 byte
+	    1, // * Infrared byte           17 - 1 byte
+	    1, // * Buttons                 18 - 1 byte
+	    2, // * Distance                19 - 2 bytes (signed)
+	    2, // * Angle                   20 - 2 bytes (signed)
+	    1, // * Charging State          21 - 1 byte
+	    2, // * Voltage                 22 - 2 bytes 
+	    2, // * Current                 23 - 2 bytes
+	    1, // * Battery Temperature     24 - 1 byte (signed)
+	    2, // * Battery Charge          25   2 bytes
+	    2, // * Battery Capacity        26 - 2 bytes
+	    2, // * Wall Signal             27 - 2 bytes
+	    2, // * Cliff Left Signal       28 - 2 bytes
+	    2, // * Cliff Left Front Signal 29 - 2 bytes
+	    2, // * Cliff Front Right Sign  30 - 2 bytes
+	    2, // * Cliff Right Signal      31 - 2 bytes
+	    1, // * Cargo Bay Digital Input 32 - 1 byte
+	    2, // * Cargo Bay Analog Signal 33 - 2 bytes
+	    1, // * Charging Sources Avail  34 - 1 byte
+	    1, // * OI Mode                 35 - 1 byte
+	    1, // * Song Number             36 - 1 byte
+	    1, // * Song Playing            37 - 1 byte
+	    1, // * Number of Stream Pkts   38 - 1 byte
+	    2, // * Requested Velocity      39 - 2 bytes (signed)
+	    2, // * Requested Radius        40 - 2 bytes (signed)
+	    2, // * Requested Right Velo    41 - 2 bytes (signed)
+	    2 // * Requested Left Velo     42 - 2 bytes (signed)
+	};
 	
 	private static final byte PACKET_START = 0x13;
 	private static final int LEN_IDX = 1;
@@ -53,6 +97,30 @@ public class SensorPacketReader {
 		}
 		
 		return true;
+	}
+	
+	public ArrayList<Integer> getPacketValues() throws InvalidPacketError {
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		int lastPosition = mPacketBuffer.position();
+		
+		mPacketBuffer.position(LEN_IDX + 1);
+		// The -1 is for the last checksum
+		while (mPacketBuffer.position() < (lastPosition - 1)) {
+			int sensorPacket = mPacketBuffer.get();
+			int numBytes = PACKET_PAYLOAD_SIZES[sensorPacket];
+			if (numBytes == 1) {
+				values.add(new Integer(mPacketBuffer.get()));
+			} else if (numBytes == 2) {
+				int value = (mPacketBuffer.get() << 8) | mPacketBuffer.get();
+				values.add(new Integer(value));
+			} else {
+				throw new InvalidPacketError("Invalid payload size: " + numBytes);
+			}
+			// Always remember to pull the checksum out
+			mPacketBuffer.get();
+		}
+				
+		return values;
 	}
 		
 	private boolean validChecksum(int packetLength) {
